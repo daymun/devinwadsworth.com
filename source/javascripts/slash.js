de5086a59db3360eba2437c1e4f05a4c2daa8583
Devin Wadsworth <devinwadsworth@gmail.com>
2012-06-04 23:12:39 -0400

    disable nav js

--- source/javascripts/slash.js
+++ source/javascripts/slash.js
@@ -12,22 +12,6 @@
 		}
 	});
 
-	/* navigation.js */
-	var appends = '<option>Menu</option>';
-
-	$('.menu .main > li').each(function(){
-		var link = $(this).children('a');
-		appends += '<option value="'+link.attr('href')+'">'+link.html()+'</option>';
-		$(this).find('li').each(function(){
-			var link = $(this).children('a');
-			appends += '<option value="'+link.attr('href')+'">- '+link.html()+'</option>';
-		});
-	});
-
-	$('nav.menu').append('<select>'+appends+'</select>').on('change', 'select', function(){
-		location.href = $(this).val();
-	});
-
 	/* caption.js */
 	$('.entry').each(function(i){
 		var _i = i;
